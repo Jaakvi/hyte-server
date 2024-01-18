5b6a19374aa4356e164382dc6754daaa443ee462
Jaakvi <jjm.virtanen@gmail.com>
2024-01-18 10:19:29 +0200

    testi

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// index.js
+import express from 'express';
+const hostname = '127.0.0.1';
+const port = 3000;
+const app = express();
+
+app.get('/items', (req, res) => {
+  res.json([
+    {id: 1, name: 'Item1'},
+    {id: 2, name: 'Item2'},
+  ]);
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my REST api');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
